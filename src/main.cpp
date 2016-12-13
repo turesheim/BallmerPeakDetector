a965d78fc5fc2bcf12816980945022169b77ebd2
Torkild U. Resheim <torkildr@gmail.com>
2016-12-13 13:54:34 +0100

    Basic publish over MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,28 @@
  *
  */
 #include <Esp.h>
+#include <stdio.h>
+
+#include <PubSubClient.h>					// Required for MQTT support
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include "../include/ESP8266mDNS.h" // bug https://github.com/esp8266/Arduino/issues/2211
+#include <ESP8266HTTPUpdateServer.h>		// Enable update over HTTP
+#include "../include/ESP8266mDNS.h" 		// Bug https://github.com/esp8266/Arduino/issues/2211
 
-#include <EEPROM.h> 				// Store some data in the EEPROM
+#include <EEPROM.h> 						// Store some data in the EEPROM
 
-#include <SFE_MicroOLED.h>  		// Support for the attached OLED screen
-#include <Wire.h>  					// Required for I2C and the screen
+#include <SFE_MicroOLED.h>  				// Support for the attached OLED screen
+#include <Wire.h>  							// Required for I2C and the screen
 
-const char* host = "bpd"; 			//!< HTTP server host name
-const int port = 80;				//!< HTTP port number
+const char* host = "bpd"; 					//!< HTTP server host name
+const int port = 80;						//!< HTTP port number
 
-const char* ssids[] = { "guest" };
-const char* passwords[] = { "password" };
+const char* ssids[] = { "" };				//!< List of WLAN SSIDs
+const char* passwords[] = { "" };			//!< List of WLAN passwords
 
-#define PIN_RESET 255  				//!< Connect RST to pin 9
+#define PIN_RESET 255  						//!< Connect RST to pin 9
 #define DC_JUMPER 0
 
 ESP8266WebServer httpServer(port);
@@ -30,7 +33,25 @@ ESP8266HTTPUpdateServer httpUpdater;
 
 MicroOLED oled(PIN_RESET, DC_JUMPER);
 
-char buffer[10];
+const char* mqttServer = "192.168.1.111";	//!< MQTT server address
+const char* mqttUser = "";					//!< MQTT client user identifier
+const char* mqttPassword = "";				//!< MQTT client password
+const long mqttPort = 1883;					//!< MQTT server port
+#define MQTTTOPIC "ballmerpeakdetector/bac" //!< MQTT topic for BAC measurement
+
+// MQTT Status
+#define MQTT_STATUS_UNKNOWN 0				//!< MQTT undefined status
+#define MQTT_STATUS_SENDING 1				//!< MQTT sending status
+#define MQTT_STATUS_ACKNOWLEDGED 2
+#define MQTT_STATUS_FAILED 3
+unsigned int mqttStatus = MQTT_STATUS_UNKNOWN;
+const char* mqttTopicInAck = MQTTTOPIC "/ack";
+const char* mqttTopicOutConnecting = MQTTTOPIC "/connecting";
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+char buffer[10];					//!< Number conversion character buffer
 /**
  * Center and print a small title
  *
@@ -65,7 +86,7 @@ void printBAC(int bac, int font) {
 	int middleY = oled.getLCDHeight() / 2;
 
 	String string = "";
-	string += itoa(bac,buffer,10);
+	string += itoa(bac, buffer, 10);
 	oled.clear(PAGE);
 	oled.setFontType(0);
 	// try to set the cursor in the middle of the screen
@@ -88,14 +109,82 @@ void printText(char* text) {
 	oled.display();
 }
 
-void handleRoot(){
-	httpServer.send(200, "text/plain", "Hello from esp8266! Go to <a href='/update'>/update</a> to upload new firmware.");
+void handleRoot() {
+	httpServer.send(200, "text/plain",
+			"Hello from esp8266! Go to <a href='/update'>/update</a> to upload new firmware.");
+}
+
+void reconnectMqtt() {
+	/*
+	 * This code is copied from an example.
+	 * Not sure it is the optimal usage of the client id.
+	 */
+
+	// Loop until we're reconnected
+	while (!mqttClient.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// Create a random client ID
+		String clientId = "bpd-";
+		clientId += String(random(0xffff), HEX);
+		// Attempt to connect
+		//if (mqttClient.connect(clientId.c_str(), mqttUser, mqttPassword)) {
+		if (mqttClient.connect(clientId.c_str())) {
+			Serial.println("connected");
+			// Once connected, publish an announcement...
+			if (mqttStatus == MQTT_STATUS_UNKNOWN) {
+				mqttClient.publish(mqttTopicOutConnecting, "BPD Starting");
+				Serial.println("Connecting");
+			} else {
+				mqttClient.publish(mqttTopicOutConnecting, "BPD Reconnecting");
+				Serial.println("Reconnecting");
+			}
+
+			// ... and resubscribe
+			if (mqttClient.subscribe(mqttTopicInAck, 1)) {
+				Serial.println("Subscribe ok");
+			} else {
+				Serial.println("Subscribe failed");
+			}
+		} else {
+			Serial.print("failed, rc=");
+			Serial.print(mqttClient.state());
+			Serial.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+	/*
+	 * Callback for subscribed messages
+	 */
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	Serial.print("] ");
+	for (int i = 0; i < length; i++) {
+		Serial.print((char) payload[i]);
+	}
+
+	// Set status to turn on green led if ack is received
+	if (strcmp(topic, mqttTopicInAck) == 0) {
+		Serial.println("MQTT_STATUS_ACKNOWLEDGED Received");
+		mqttStatus = MQTT_STATUS_ACKNOWLEDGED;
+	}
+
+}
+
+void writeMqtt(int bac) {
+	mqttStatus = MQTT_STATUS_SENDING;
+	reconnectMqtt();
+	Serial.println("Sending MQTT data");
+	mqttClient.publish(MQTTTOPIC, String(bac).c_str());
 }
 
 void setup() {
-
+	Serial.begin(9600);
 	// handle the root of the web server
-	httpServer.on("/", HTTP_GET, handleRoot);
+	//httpServer.on("/", HTTP_GET, handleRoot);
 
 	oled.begin();    	// initialize the OLED
 	oled.clear(ALL); 	// clear the display's internal memory
@@ -103,8 +192,9 @@ void setup() {
 
 	// read the current AP index from flash memory
 	EEPROM.begin(64);
-	int i = (int)EEPROM.read(0);
-	if (i > (sizeof(ssids)/sizeof(*ssids))) i=0;
+	int i = (int) EEPROM.read(0);
+	if (i > (sizeof(ssids) / sizeof(*ssids)))
+		i = 0;
 
 	// connect to the AP
 	printText("Connecting\n");
@@ -112,9 +202,9 @@ void setup() {
 	WiFi.mode(WIFI_AP_STA);
 	WiFi.begin(ssids[i], passwords[i]);
 	while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-		printTitle("Failed!",0);
+		printTitle("Failed!", 0);
 		i++; // try the next access point
-		EEPROM.write(0,i);
+		EEPROM.write(0, i);
 		EEPROM.commit();
 		delay(2000);
 		ESP.restart();
@@ -124,21 +214,27 @@ void setup() {
 	httpUpdater.setup(&httpServer);
 	httpServer.begin();
 
-	MDNS.addService("http","tcp",port);
+	MDNS.addService("http", "tcp", port);
+
+	// MQTT
+	mqttClient.setServer(mqttServer, mqttPort);
+	mqttClient.setCallback(mqttCallback);
 
 	// prepare to read the analog input
-	pinMode(A0,INPUT);
+	pinMode(A0, INPUT);
 }
 
 int out;
 void loop() {
 	IPAddress a = WiFi.localIP();
-	for (int i=0;i<2000;i++){
-		httpServer.handleClient();
-		delay(1);
-	}
+//	for (int i = 0; i < 2000; i++) {
+//		httpServer.handleClient();
+//		delay(1);
+//	}
+	delay(2000);
 	out = (unsigned int) analogRead(A0);
-	printBAC(out,1);
-
+	printBAC(out, 1);
+	writeMqtt(out);
+	mqttClient.loop();
 }
 
